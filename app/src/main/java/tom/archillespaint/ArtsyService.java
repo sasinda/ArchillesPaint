90285bb8f7b3c87e6cbddf458ad01507d34c1081
Sasinda <sp2335@cornell.edu>
2017-01-09 19:49:32 +0200

    Image effects and buttons

--- app/src/main/java/tom/archillespaint/ArtsyService.java
+++ app/src/main/java/tom/archillespaint/ArtsyService.java
@@ -1,5 +1,12 @@
 package tom.archillespaint;
 
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import jp.wasabeef.picasso.transformations.gpu.KuwaharaFilterTransformation;
+
 /**
  * Created by sasinda on 1/9/17.
  */
@@ -9,5 +16,16 @@ public class ArtsyService {
 
     public String getImgURL(){
         return null;
+    }
+
+    public ImageView loadImage(ImageView imgView, Context context){
+
+        Picasso.with(context).load("http://previews.123rf.com/images/kabby/kabby1006/kabby100600002/7090722-Abstract-fun-cheerful-artsy-rainbow-backgrounds-design-Stock-Photo.jpg")
+//                .transform(new SketchFilterTransformation(context))
+                .transform(new KuwaharaFilterTransformation(context, 10))
+                .error(R.drawable.aw_snap)
+                .into(imgView);
+
+        return imgView;
     }
 }
